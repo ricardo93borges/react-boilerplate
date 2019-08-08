722684538740d2330691b3b9bce0b6574931910a
olaviolacerda <olavio.lacerda@email.com>
2019-08-08 01:14:48 -0300

    added tests to __tests__

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App from '../App';
 
 describe('App test', () => {
   it('renders without crashing', () => {
