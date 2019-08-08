239840ec4a082dd8380485df7f5f64e11ebfd213
olaviolacerda <olavio.lacerda@email.com>
2019-08-08 01:03:31 -0300

    :white_check_mark: added jest-enzyme as a dependency

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
-/* eslint-disable no-undef */
 import React from 'react';
-import { shallow } from '../setupTests';
-
+import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App test', () => {
-  it('Should render app', () => {
-    const app = shallow(<App />);
-
-    expect(app.exists()).toBeTruthy();
+  it('renders without crashing', () => {
+    shallow(<App />);
   });
 });
